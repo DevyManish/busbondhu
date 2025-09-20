c4e2796a148072eafe40c7f3b3a6591b664278e9
DevyManish <manishguptacpn@gmail.com>
2025-09-20 15:51:02 +0530

    ui: add app logo & favicon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Welcome to Busbondhu!
       </Text>
     </View>
   );
