2445d1069dc716faefadbfc3247241a18fc0284f
DevyManish <manishguptacpn@gmail.com>
2025-09-20 15:06:46 +0530

    init: create expo app with nativewind & gluestack

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,49 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="home-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="search-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "Setting",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="settings-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Setting() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Settings!
+      </Text>
+    </View>
+  );
+}
